f080fa711423f9e9d231b90ce9b4823cceb207a6
GaneshMumbai2001 <ganeshmumbai2001@gmail.com>
2024-12-29 18:09:24 +0530

    finished frontend

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,12 @@
-import { Outlet} from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import React from "react";
+
 const Layout = () => {
     return (
         <main>
             <Outlet />
-            
         </main>
     );
-}
+};
+
+export default Layout;  // This is correct for default export
